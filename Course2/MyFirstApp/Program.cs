ae86dbdae71da68ba75f001ac60fa8400969cb53
mirolimmajidov <mirolimmajidov@outlook.com>
2025-02-27 20:10:30 +0500

    Added soe logic

--- Course2/MyFirstApp/Program.cs
+++ Course2/MyFirstApp/Program.cs
@@ -1,4 +1,50 @@
-﻿Console.WriteLine("Enter your name: ");
+﻿//Console.WriteLine("Enter your name: ");
 
-var name = Console.ReadLine();
-Console.WriteLine($"Hello {name}!");
+// string name = "Jake";
+// int age = 1111111111;
+// age = 4444;
+// int age2 = age;
+// const int age3 = 22;
+//
+// string ageAsString = age.ToString();
+// int ageAsInt = int.Parse(ageAsString);
+// long age1 = 4545545484545454545;
+// DateOnly birthday = new DateOnly(19, 1, 1);
+// DateTime now = DateTime.Now;
+// decimal test = 1.2m;
+// float test2 = 1.2f;
+// double test3 = 1.2d;
+// test3 = age;
+// short test4 = 4444;
+// Console.WriteLine(now);
+
+// var jakeName = "Jake";
+// var myName = "Ali";
+// var myAge = 15;
+// var myText = $"Hi {jakeName}, my name is {myName}. I am {myAge} years old.";
+// Console.WriteLine(myText);
+//
+// string[] names = { "Ali", "Vali" };
+//
+// int[] ages = [12, 45];
+// var names2 = new string[3];
+// names2[0] = "Ali";
+// names2[1] = "Vali";
+// // names2[2] = "Joni";
+// var name = names[1];
+//
+// foreach (var _name in names2)
+//     Console.WriteLine(_name);
+
+
+bool enabled = true;
+if (enabled)
+{
+    Console.WriteLine("Enabled");
+}
+else
+{
+    Console.WriteLine("Disabled");
+}
+
+Console.WriteLine("End");
