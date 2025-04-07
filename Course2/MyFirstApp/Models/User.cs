e39141fb3ea4e23dd5130b8220c3d37583e023e5
mirolimmajidov <mirolimmajidov@outlook.com>
2025-04-07 20:06:29 +0500

    Added some example

--- Course2/MyFirstApp/Models/User.cs
+++ Course2/MyFirstApp/Models/User.cs
@@ -4,7 +4,7 @@ internal class User
 {
     public static EventHandler<DoWorkEventArgs> UserCreated;
     public EventHandler OnStartingWork;
-    
+
     public int Id { get; }
 
     private string _firstName;
@@ -29,15 +29,17 @@ internal class User
         }
     }
 
-    public string LastName { get;  set; }
+    public string LastName { get; set; }
     public string Email { get; set; }
-    
+
     public string Password { get; set; }
     public int Age { get; set; }
-    
+
     public int? Level { get; set; }
 
     public static int Counter { get; set; }
+
+    public int Balance { get; set; }
 
     public User()
     {
@@ -49,10 +51,10 @@ internal class User
         {
             UserName = Email
         };
-        
+
         UserCreated?.Invoke(this, eventArgs);
     }
-    
+
     // public User(Notifier notifier)
     // {
     //     Email = "User";
@@ -89,19 +91,36 @@ internal class User
     ~User()
     {
     }
+    
+    Lock _lockBalance = new Lock();
+    public void Withdraw(int amount)
+    {
+        lock (_lockBalance)
+        {
+            if (Balance > 0)
+            {
+                if (Balance >= amount)
+                {
+                    Balance -= amount;
+                }
+                else
+                {
+                    throw new Exception("Not enough balance.");
+                }
+            }
+        }
+    }
 }
 
 public class TestClass(int Id, string FirstName, string LastName, string Email, string Password);
-
 
 public class SmsSender
 {
     public static void Send(string message)
     {
-        
     }
+
     public void Send2(string message)
     {
-        
     }
 }
---
+++ Course2/MyFirstApp/ProgramOld.cs
@@ -0,0 +1,148 @@
+﻿// Thread t1 = new Thread(MyJob);
+// t1.Start(1);
+// Thread t2 = new Thread(MyJob);
+// t2.Start(2);
+
+using System.Collections.Concurrent;
+using System.Diagnostics;
+using MyFirstApp.Models;
+
+public class ProgramOld
+{
+    internal static ConcurrentQueue<User> Users = new ConcurrentQueue<User>();
+    public static SemaphoreSlim _pool = new SemaphoreSlim(initialCount: 0);
+
+    public static async Task Main()
+    { 
+        var timer = Stopwatch.StartNew();
+        
+        // using var timeoutCancellationTokenSource = new CancellationTokenSource();
+        // //timeoutCancellationTokenSource.CancelAfter(TimeSpan.FromSeconds(5));
+        // var cancellationToken = timeoutCancellationTokenSource.Token;
+        //
+        // Task task4 = Task.Run(() =>
+        // {
+        //     while (true)
+        //     {
+        //         Thread.Sleep(1000);
+        //         Console.WriteLine($"Working...{DateTime.Now} ");
+        //         if (cancellationToken.IsCancellationRequested)
+        //         {
+        //             //cancellationToken.ThrowIfCancellationRequested();
+        //             break;
+        //         }
+        //
+        //         //cancellationToken.ThrowIfCancellationRequested();
+        //         Task.Delay(1000, cancellationToken);
+        //     }
+        //
+        // }, cancellationToken);
+        //
+        // var counter = 0;
+        //
+        // while (task4.IsCompleted == false)
+        // {
+        //     counter++;
+        //     await Task.Delay(500, cancellationToken);
+        //     if(counter == 10)
+        //         await timeoutCancellationTokenSource.CancelAsync();
+        // }
+        // await task4;
+        //Parallel.For(1, 5, Square);
+
+        // ParallelLoopResult result = Parallel.ForEach<int>(
+        //        new List<int>() { 1, 3, 5, 8 },
+        //        Square
+        // );
+
+        // await Task.Run(() => MyTask(1, 2));
+        MyTask(2, 1);
+        // await Task.Run(() => MyTask(2, 1));
+        // await Task.Run(() => MyTask(3, 3));
+        // var task1 = Task.Run(() => MyTask(1, 2));
+        // var task2 = Task.Run(() => MyTask(2, 1));
+        // var task3 = Task.Run(() => MyTask(3, 3));
+        // await task1.ContinueWith(task => { Console.WriteLine($"Task1: {task1.IsCompleted}"); });
+        //
+        // var task = new Task(() => MyTask(4, 1));
+        // task.Start();
+        // await task2.ConfigureAwait(false);
+        //
+        // var task5 = Task.Factory.StartNew(() => { }, TaskCreationOptions.LongRunning);
+        
+        // var result = await Task.Run(() => MyTask(1, 2));
+        //Task.WaitAll(task1, task2, task3);
+        //var allTasks = Task.WhenAll(task1, task2, task3);
+
+        // var resultValue3 = await task1;
+        //var resultValue1 = task1.GetAwaiter().GetResult();
+        // var resultValue2 = task1.Result;
+        //
+        // await task1;
+        // await task2;
+        // // await Task.Run(() => MyJob(3));
+        // // await Task.Run(() => MyJob(3));
+        // //await MyFirstTask();
+        // task1.GetAwaiter().GetResult();
+        //
+        // Task.WaitAll(task1, task2, task3);
+        // timer.Stop();
+        // Console.WriteLine($"Total time: {timer.Elapsed}");
+        //
+        // var task4 = Task.Run(() => MyJob(4));
+
+        // for (int i = 1; i <= 10; i++)
+        // {
+        //     Users.Enqueue(new User(i, $"User {i}"));
+        //     
+        //     Thread t = new Thread(Worker);
+        //     t.Start();
+        // }
+
+        Thread.Sleep(500);
+
+        //Console.WriteLine("Main thread calls Release(3).");
+        _pool.Release(releaseCount: 3);
+
+        Console.WriteLine("Main thread exits.");
+    }
+
+    static void Square(int n)
+    {
+        Console.WriteLine($"Result of {n} is {n * n}");
+        Thread.Sleep(TimeSpan.FromSeconds(0.5));
+        Console.WriteLine($"Completed task: {Task.CurrentId}, {Thread.CurrentThread.ManagedThreadId}");
+    }
+
+    static Task<int> MyTask(int id, int seconds)
+    {
+        return Task.Run(() =>
+        {
+            Console.WriteLine("MyJob ({1}) is running on thread {0}", Thread.CurrentThread.ManagedThreadId, id);
+            Thread.Sleep(seconds * 1000);
+            Console.WriteLine("MyJob ({0}) completed", id);
+
+            return seconds * 2;
+        });
+    }
+
+
+    static void Worker()
+    {
+        _pool.Wait();
+
+        Users.TryDequeue(out User user);
+        Console.WriteLine("User {0} enters the semaphore.", user!.Id);
+
+        Thread.Sleep(1000);
+
+        Console.WriteLine("User {0} releases the semaphore.", user!.Id);
+        _pool.Release();
+    }
+
+    static void MyJob(object id)
+    {
+        Console.WriteLine("MyJob ({1}) is running on thread {0}", Thread.CurrentThread.ManagedThreadId, id);
+        Thread.Sleep(1000);
+    }
+}
