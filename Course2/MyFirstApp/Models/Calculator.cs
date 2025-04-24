d75e4506cb2bbea527f39ac3066b02fea1a9a338
mirolimmajidov <mirolimmajidov@outlook.com>
2025-04-24 18:59:57 +0500

    Added Generic type for the method

--- Course2/MyFirstApp/Models/Calculator.cs
+++ Course2/MyFirstApp/Models/Calculator.cs
@@ -1,28 +1,32 @@
+using System.Numerics;
+
 namespace MyFirstApp.Models;
 
 public class Calculator
 {
-    public double Add(double a, double b)
+    public T Add<T>(T a, T b)
+        where T : INumber<T>
     {
         return a + b;
     }
 
-    public double Subtract(double a, double b)
+    public int Subtract(int a, int b)
     {
         return a - b;
     }
 
-    public double Multiply(double a, double b)
+    public int Multiply(int a, int b)
     {
         return a * b;
     }
 
-    public double Divide(double a, double b)
+    public int Divide(int a, int b)
     {
         if (b == 0)
         {
             throw new DivideByZeroException("Cannot divide by zero.");
         }
+
         return a / b;
     }
 }
--- Course2/MyFirstApp/Program.cs
+++ Course2/MyFirstApp/Program.cs
@@ -1,26 +1,9 @@
 ﻿using System.Collections.Concurrent;
 using MyFirstApp.Models;
 
-Dictionary<string, string> dic = new Dictionary<string, string>();
-dic.Add("Key", "Value");
+var cal = new Calculator();
+int a = 15;
+int b = 10;
 
-var dic2 = new ConcurrentDictionary<string, string>();
-dic2.TryAdd("Key", "Value");
-
-var user = new User() { Balance = 100 };
-
-var task1 = Task.Run(() =>
-{
-    user.Withdraw(50);
-});
-var task2 = Task.Run(() =>
-{
-    user.Withdraw(50);
-});
-var task3 = Task.Run(() =>
-{
-    user.Withdraw(50);
-});
-
-Task.WaitAll(task1, task2, task3);
-Console.WriteLine(user.Balance);
+var result = cal.Add(a, b);
+Console.WriteLine(result);
