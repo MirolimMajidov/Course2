ad36ee8e7ccb91e320b678aca7de6245f447d29e
mirolimmajidov <mirolimmajidov@outlook.com>
2025-05-06 08:45:40 +0500

    added restriction

--- Course2/BankManagementSystem/DTOs/ClientDTOs/CreateClient.cs
+++ Course2/BankManagementSystem/DTOs/ClientDTOs/CreateClient.cs
@@ -1,10 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+
 namespace BankManagementSystem.DTOs.ClientDTOs;
 
 //public record CreateClient(string FirstName, string LastName, int Age, string Email);
 public record CreateClient
 {
+    //[FromRoute]
+    [Required, MinLength(3), MaxLength(30)]
     public string FirstName { get; init; }
+    
+    //[FromHeader]
     public string LastName { get; init; }
+    
+    //[FromForm]
     public int Age { get; init; }
+    
+    [EmailAddress]
     public string Email { get; init; }
 }
