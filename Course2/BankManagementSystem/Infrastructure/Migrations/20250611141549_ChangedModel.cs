b5a4cd702e8eff474eaea9313c3d9f1cfe163a4e
mirolimmajidov <mirolimmajidov@outlook.com>
2025-06-11 19:59:19 +0500

    added migration

---
+++ Course2/BankManagementSystem/Infrastructure/Migrations/20250611141549_ChangedModel.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BankManagementSystem.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangedModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_People_Nickname",
+                table: "People");
+
+            migrationBuilder.RenameColumn(
+                name: "Nickname",
+                table: "People",
+                newName: "UserName");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_People_UserName",
+                table: "People",
+                column: "UserName",
+                unique: true,
+                filter: "[UserName] IS NOT NULL");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_People_UserName",
+                table: "People");
+
+            migrationBuilder.RenameColumn(
+                name: "UserName",
+                table: "People",
+                newName: "Nickname");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_People_Nickname",
+                table: "People",
+                column: "Nickname",
+                unique: true,
+                filter: "[Nickname] IS NOT NULL");
+        }
+    }
+}
