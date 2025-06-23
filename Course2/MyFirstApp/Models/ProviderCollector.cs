1b37bec8dd8edbef4c4feda904b5aa25e54059ea
mirolimmajidov <mirolimmajidov@outlook.com>
2025-06-23 19:58:23 +0500

    Implement a Dispose method

---
+++ Course2/MyFirstApp/Models/ProviderCollector.cs
@@ -0,0 +1,5 @@
+namespace MyFirstApp.Models;
+
+public class ProviderCollector : List<DatabaseProvider>
+{
+}
--- Course2/MyFirstApp/Program.cs
+++ Course2/MyFirstApp/Program.cs
@@ -1,13 +1,12 @@
-﻿using System.Collections.Concurrent;
-using MyFirstApp.Models;
+﻿using MyFirstApp.Models;
 
-var cal = new Calculator<int>();
-cal.MultiplyCount = 3;
-int a = 2;
-int b = 3;
+var collector = new ProviderCollector();
 
-var sum = cal.Add(a, b);
-// var result = cal.Subtract(a, b);
 
-var result = cal.Multiply(a, b);
-Console.WriteLine(result);
+using (var provider = new DatabaseProvider(collector))
+{
+    var tablesCount = provider.GetTablesCount();
+    Console.WriteLine(tablesCount);
+}
+
+Console.WriteLine("Press any key to exit...");;
