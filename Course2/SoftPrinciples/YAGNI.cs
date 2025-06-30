e2f4b6a816b17992a53ea33919e8c4ae3075bbff
mirolimmajidov <mirolimmajidov@outlook.com>
2025-06-30 20:07:04 +0500

    Added example for YAGNI

--- Course2/SoftPrinciples/YAGNI.cs
+++ Course2/SoftPrinciples/YAGNI.cs
@@ -1,4 +1,30 @@
 ﻿namespace YAGNI
 {
-   
+    public class Task
+    {
+        public string Title { get; set; }
+        public bool IsCompleted { get; set; }
+
+        // Not needed now, but added "just in case"
+        public int Priority { get; set; }
+        public DateTime? NotificationTime { get; set; }
+        public string SharedWith { get; set; }
+
+        public void NotifyUser()
+        {
+            // Placeholder for future functionality
+            Console.WriteLine("Notify user at " + NotificationTime);
+        }
+
+        public void ShareTask()
+        {
+            Console.WriteLine("Sharing with: " + SharedWith);
+        }
+    }
+    
+    public class Task2
+    {
+        public string Title { get; set; }
+        public bool IsCompleted { get; set; }
+    }
 }
