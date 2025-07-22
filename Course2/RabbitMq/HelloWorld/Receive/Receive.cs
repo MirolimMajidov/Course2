e2a477bcece87417e8cc2403ba607a1ae71654e7
Mirolim Majidov <mirolimmajidov@outlook.com>
2025-07-22 11:29:16 +0500

    added a new projects

---
+++ Course2/RabbitMq/HelloWorld/Receive/Receive.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Receiver
+{
+    class Program
+    {
+
+        public static async Task Main(string[] args)
+        {
+            var factory = new ConnectionFactory
+            {
+                HostName = "localhost",
+                Port = 5672,
+                UserName = "admin",
+                Password = "admin123"
+            };
+            await using var connection = await factory.CreateConnectionAsync();
+            await using var channel = await connection.CreateChannelAsync();
+
+            await channel.QueueDeclareAsync(queue: "ItRun", durable: false, exclusive: false, autoDelete: false,
+                arguments: null);
+
+            Console.WriteLine(" [*] Waiting for messages.");
+
+            var consumer = new AsyncEventingBasicConsumer(channel);
+            consumer.ReceivedAsync += (model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine($" [x] Received {message}");
+                return Task.CompletedTask;
+            };
+
+            await channel.BasicConsumeAsync("ItRun", autoAck: true, consumer: consumer);
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Course2/RabbitMq/HelloWorld/Send/Send.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+
+namespace Sender
+{
+    class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var factory = new ConnectionFactory
+            {
+                HostName = "localhost",
+                Port = 5672,
+                UserName = "admin",
+                Password = "admin123"
+            };
+            await using var connection = await factory.CreateConnectionAsync();
+            await using var channel = await connection.CreateChannelAsync();
+
+            await channel.QueueDeclareAsync(queue: "ItRun", durable: false, exclusive: false, autoDelete: false,
+                arguments: null);
+
+            while (true)
+            {
+                Console.WriteLine("Enter your name:");
+                var name = Console.ReadLine(); 
+                string message = $"Hello {name}!";
+                var body = Encoding.UTF8.GetBytes(message);
+
+                await channel.BasicPublishAsync(exchange: string.Empty, routingKey: "ItRun", body: body);
+                Console.WriteLine($" [x] Sent {message}");
+            }
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+}
